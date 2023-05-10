628147678b5260c8f14737a14f6b80c3ec70b48f
KIMPPT <dudghks9303@gmail.com>
2023-05-10 13:17:31 +0900

    05101317

--- src/components/BoardCard.jsx
+++ src/components/BoardCard.jsx
@@ -9,6 +9,7 @@ props에 board로 들고 왔다
 그리고 현재 컴포넌트에서 board를 쓴 것이다
 */
 export default function BoardCard({ board }) {
+  console.log(board);
   return (
     <Card style={{ width: "18rem", margin: "auto", marginTop:"20px"}}>
       <Card.Body>
